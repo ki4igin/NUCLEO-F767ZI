4beddf1155549ed1431717d6dc16657fdd4a2523
artem <ki4igin1992@outlook.com>
2023-07-13 23:59:13 +0300

    add uarts for modbus

--- udp_server/code/Src/usart.c
+++ udp_server/code/Src/usart.c
@@ -1,87 +1,271 @@
-/* USER CODE BEGIN Header */
+
 /**
-  ******************************************************************************
-  * @file    usart.c
-  * @brief   This file provides code for the configuration
-  *          of the USART instances.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
+ ******************************************************************************
+ * @file    usart.c
+ * @brief   This file provides code for the configuration
+ *          of the USART instances.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
 /* Includes ------------------------------------------------------------------*/
 #include "usart.h"
+#include "tools.h"
 
-/* USER CODE BEGIN 0 */
+#define BUF_SIZE 32
 
-/* USER CODE END 0 */
+uint8_t uart4_buf_tx[BUF_SIZE];
+uint8_t uart4_buf_rx[BUF_SIZE];
+uint8_t uart7_buf_tx[BUF_SIZE];
+uint8_t uart7_buf_rx[BUF_SIZE];
 
 /* USART3 init function */
-
 void MX_USART3_UART_Init(void)
 {
+    LL_USART_InitTypeDef USART_InitStruct = {0};
 
-  /* USER CODE BEGIN USART3_Init 0 */
+    LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  /* USER CODE END USART3_Init 0 */
+    LL_RCC_SetUSARTClockSource(LL_RCC_USART3_CLKSOURCE_PCLK1);
 
-  LL_USART_InitTypeDef USART_InitStruct = {0};
+    /* Peripheral clock enable */
+    LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_USART3);
 
-  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOD);
+    /**USART3 GPIO Configuration
+    PD8   ------> USART3_TX
+    PD9   ------> USART3_RX
+    */
+    GPIO_InitStruct.Pin = STLK_RX_Pin;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    GPIO_InitStruct.Alternate = LL_GPIO_AF_7;
+    LL_GPIO_Init(STLK_RX_GPIO_Port, &GPIO_InitStruct);
 
-  LL_RCC_SetUSARTClockSource(LL_RCC_USART3_CLKSOURCE_PCLK1);
+    GPIO_InitStruct.Pin = STLK_TX_Pin;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    GPIO_InitStruct.Alternate = LL_GPIO_AF_7;
+    LL_GPIO_Init(STLK_TX_GPIO_Port, &GPIO_InitStruct);
 
-  /* Peripheral clock enable */
-  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_USART3);
-
-  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOD);
-  /**USART3 GPIO Configuration
-  PD8   ------> USART3_TX
-  PD9   ------> USART3_RX
-  */
-  GPIO_InitStruct.Pin = STLK_RX_Pin;
-  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-  GPIO_InitStruct.Alternate = LL_GPIO_AF_7;
-  LL_GPIO_Init(STLK_RX_GPIO_Port, &GPIO_InitStruct);
-
-  GPIO_InitStruct.Pin = STLK_TX_Pin;
-  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-  GPIO_InitStruct.Alternate = LL_GPIO_AF_7;
-  LL_GPIO_Init(STLK_TX_GPIO_Port, &GPIO_InitStruct);
-
-  /* USER CODE BEGIN USART3_Init 1 */
-
-  /* USER CODE END USART3_Init 1 */
-  USART_InitStruct.BaudRate = 115200;
-  USART_InitStruct.DataWidth = LL_USART_DATAWIDTH_8B;
-  USART_InitStruct.StopBits = LL_USART_STOPBITS_1;
-  USART_InitStruct.Parity = LL_USART_PARITY_NONE;
-  USART_InitStruct.TransferDirection = LL_USART_DIRECTION_TX_RX;
-  USART_InitStruct.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
-  USART_InitStruct.OverSampling = LL_USART_OVERSAMPLING_16;
-  LL_USART_Init(USART3, &USART_InitStruct);
-  LL_USART_ConfigAsyncMode(USART3);
-  LL_USART_Enable(USART3);
-  /* USER CODE BEGIN USART3_Init 2 */
-
-  /* USER CODE END USART3_Init 2 */
-
+    USART_InitStruct.BaudRate = 115200;
+    USART_InitStruct.DataWidth = LL_USART_DATAWIDTH_8B;
+    USART_InitStruct.StopBits = LL_USART_STOPBITS_1;
+    USART_InitStruct.Parity = LL_USART_PARITY_NONE;
+    USART_InitStruct.TransferDirection = LL_USART_DIRECTION_TX_RX;
+    USART_InitStruct.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
+    USART_InitStruct.OverSampling = LL_USART_OVERSAMPLING_16;
+    LL_USART_Init(USART3, &USART_InitStruct);
+    LL_USART_ConfigAsyncMode(USART3);
+    LL_USART_Enable(USART3);
 }
 
-/* USER CODE BEGIN 1 */
+/* UART4 init function */
+void MX_UART4_Init(void)
+{
+    LL_USART_InitTypeDef USART_InitStruct = {0};
 
-/* USER CODE END 1 */
+    LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    LL_RCC_SetUARTClockSource(LL_RCC_UART4_CLKSOURCE_PCLK1);
+
+    /* Peripheral clock enable */
+    LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_UART4);
+
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+    /**UART4 GPIO Configuration
+    PB14   ------> UART4_DE
+    PA11   ------> UART4_RX
+    PA12   ------> UART4_TX
+    */
+    GPIO_InitStruct.Pin = LL_GPIO_PIN_14;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    GPIO_InitStruct.Alternate = LL_GPIO_AF_8;
+    LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = LL_GPIO_PIN_11;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    GPIO_InitStruct.Alternate = GPIO_AF6_UART4;
+    LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = LL_GPIO_PIN_12;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    GPIO_InitStruct.Alternate = GPIO_AF6_UART4;
+    LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /* UART4 DMA Init */
+
+    /* UART4_RX Init */
+    LL_DMA_SetChannelSelection(DMA1, LL_DMA_STREAM_2, LL_DMA_CHANNEL_4);
+    LL_DMA_ConfigTransfer(
+        DMA1,
+        LL_DMA_STREAM_2,
+        LL_DMA_DIRECTION_PERIPH_TO_MEMORY | LL_DMA_PRIORITY_LOW
+            | LL_DMA_MODE_NORMAL | LL_DMA_PERIPH_NOINCREMENT
+            | LL_DMA_MEMORY_INCREMENT | LL_DMA_PDATAALIGN_BYTE
+            | LL_DMA_MDATAALIGN_BYTE);
+    LL_DMA_ConfigAddresses(
+        DMA1,
+        LL_DMA_STREAM_2,
+        LL_USART_DMA_GetRegAddr(UART4, LL_USART_DMA_REG_DATA_RECEIVE),
+        (uint32_t)uart4_buf_rx,
+        LL_DMA_GetDataTransferDirection(DMA1, LL_DMA_STREAM_2));
+    LL_DMA_SetDataLength(DMA1, LL_DMA_STREAM_2, arrlen(uart4_buf_rx));
+
+    /* UART4_TX Init */
+    LL_DMA_SetChannelSelection(DMA1, LL_DMA_STREAM_4, LL_DMA_CHANNEL_4);
+    LL_DMA_ConfigTransfer(
+        DMA1,
+        LL_DMA_STREAM_4,
+        LL_DMA_DIRECTION_MEMORY_TO_PERIPH | LL_DMA_PRIORITY_LOW
+            | LL_DMA_MODE_NORMAL | LL_DMA_PERIPH_NOINCREMENT
+            | LL_DMA_MEMORY_INCREMENT | LL_DMA_PDATAALIGN_BYTE
+            | LL_DMA_MDATAALIGN_BYTE);
+
+    USART_InitStruct.BaudRate = 115200;
+    USART_InitStruct.DataWidth = LL_USART_DATAWIDTH_8B;
+    USART_InitStruct.StopBits = LL_USART_STOPBITS_1;
+    USART_InitStruct.Parity = LL_USART_PARITY_EVEN;
+    USART_InitStruct.TransferDirection = LL_USART_DIRECTION_TX_RX;
+    USART_InitStruct.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
+    USART_InitStruct.OverSampling = LL_USART_OVERSAMPLING_16;
+    LL_USART_Init(UART4, &USART_InitStruct);
+    LL_USART_EnableDEMode(UART4);
+    LL_USART_SetDESignalPolarity(UART4, LL_USART_DE_POLARITY_HIGH);
+    LL_USART_SetDEAssertionTime(UART4, 0);
+    LL_USART_SetDEDeassertionTime(UART4, 0);
+    LL_USART_ConfigAsyncMode(UART4);
+
+    LL_USART_EnableDMAReq_TX(UART4);
+    LL_USART_EnableDMAReq_RX(UART4);
+    LL_DMA_EnableStream(DMA1, LL_DMA_STREAM_2);
+
+    LL_USART_Enable(UART4);
+}
+
+/* UART7 init function */
+void MX_UART7_Init(void)
+{
+    LL_USART_InitTypeDef USART_InitStruct = {0};
+
+    LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    LL_RCC_SetUARTClockSource(LL_RCC_UART7_CLKSOURCE_PCLK1);
+
+    /* Peripheral clock enable */
+    LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_UART7);
+
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOE);
+    /**UART7 GPIO Configuration
+    PE7   ------> UART7_RX
+    PE8   ------> UART7_TX
+    PE9   ------> UART7_DE
+    */
+    GPIO_InitStruct.Pin = LL_GPIO_PIN_7;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    GPIO_InitStruct.Alternate = LL_GPIO_AF_8;
+    LL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = LL_GPIO_PIN_8;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    GPIO_InitStruct.Alternate = LL_GPIO_AF_8;
+    LL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = LL_GPIO_PIN_9;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    GPIO_InitStruct.Alternate = LL_GPIO_AF_8;
+    LL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+    /* UART7 DMA Init */
+
+    /* UART7_RX Init */
+    LL_DMA_SetChannelSelection(DMA1, LL_DMA_STREAM_3, LL_DMA_CHANNEL_5);
+    LL_DMA_ConfigTransfer(
+        DMA1,
+        LL_DMA_STREAM_3,
+        LL_DMA_DIRECTION_PERIPH_TO_MEMORY | LL_DMA_PRIORITY_LOW
+            | LL_DMA_MODE_NORMAL | LL_DMA_PERIPH_NOINCREMENT
+            | LL_DMA_MEMORY_INCREMENT | LL_DMA_PDATAALIGN_BYTE
+            | LL_DMA_MDATAALIGN_BYTE);
+
+    /* UART7_TX Init */
+    LL_DMA_SetChannelSelection(DMA1, LL_DMA_STREAM_1, LL_DMA_CHANNEL_5);
+    LL_DMA_ConfigTransfer(
+        DMA1,
+        LL_DMA_STREAM_1,
+        LL_DMA_DIRECTION_MEMORY_TO_PERIPH | LL_DMA_PRIORITY_LOW
+            | LL_DMA_MODE_NORMAL | LL_DMA_PERIPH_NOINCREMENT
+            | LL_DMA_MEMORY_INCREMENT | LL_DMA_PDATAALIGN_BYTE
+            | LL_DMA_MDATAALIGN_BYTE);
+
+    USART_InitStruct.BaudRate = 115200;
+    USART_InitStruct.DataWidth = LL_USART_DATAWIDTH_8B;
+    USART_InitStruct.StopBits = LL_USART_STOPBITS_1;
+    USART_InitStruct.Parity = LL_USART_PARITY_EVEN;
+    USART_InitStruct.TransferDirection = LL_USART_DIRECTION_TX_RX;
+    USART_InitStruct.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
+    USART_InitStruct.OverSampling = LL_USART_OVERSAMPLING_16;
+    LL_USART_Init(UART7, &USART_InitStruct);
+    LL_USART_EnableDEMode(UART7);
+    LL_USART_SetDESignalPolarity(UART7, LL_USART_DE_POLARITY_HIGH);
+    LL_USART_SetDEAssertionTime(UART7, 0);
+    LL_USART_SetDEDeassertionTime(UART7, 0);
+    LL_USART_ConfigAsyncMode(UART7);
+    LL_USART_Enable(UART7);
+}
+
+void uart4_send_array_dma(void *buf, uint32_t size)
+{
+    LL_DMA_ConfigAddresses(
+        DMA1,
+        LL_DMA_STREAM_4,
+        (uint32_t)buf,
+        LL_USART_DMA_GetRegAddr(UART4, LL_USART_DMA_REG_DATA_TRANSMIT),
+        LL_DMA_GetDataTransferDirection(DMA1, LL_DMA_STREAM_4));
+    LL_DMA_SetDataLength(DMA1, LL_DMA_STREAM_4, size);
+    LL_DMA_EnableStream(DMA1, LL_DMA_STREAM_4);
+}
+
+void uart7_send_array_dma(void *buf, uint32_t size)
+{
+    LL_DMA_ConfigAddresses(
+        DMA1,
+        LL_DMA_STREAM_1,
+        (uint32_t)buf,
+        LL_USART_DMA_GetRegAddr(UART7, LL_USART_DMA_REG_DATA_TRANSMIT),
+        LL_DMA_GetDataTransferDirection(DMA1, LL_DMA_STREAM_1));
+    LL_DMA_SetDataLength(DMA1, LL_DMA_STREAM_1, size);
+    LL_DMA_EnableStream(DMA1, LL_DMA_STREAM_1);
+}
