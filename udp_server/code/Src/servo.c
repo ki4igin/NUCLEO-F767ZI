d0ee1f9872554c84d69ff02c747fbb73552adfe0
artem <ki4igin1992@outlook.com>
2023-07-27 01:17:56 +0300

    fff

--- udp_server/code/Src/servo.c
+++ udp_server/code/Src/servo.c
@@ -4,21 +4,30 @@
 #define SERVO_PERIOD_PWM_US 20000
 
 struct servo {
-    volatile uint32_t *tim_ccr;
-    volatile uint32_t *tim_arr;
-    uint32_t tim_ccr_min;
-    uint32_t tim_ccr_range;
+    struct tim {
+        volatile uint32_t *const ccr;
+        uint32_t ccr_val_min;
+        uint32_t ccr_vals_range;
+    } tim;
+
+    int32_t cal_offset;
 };
 
-void servo_init(struct servo *s, uint32_t pwm_width_min_us, uint32_t pwm_width_max_us)
+void servo_init(
+    struct servo *s,
+    uint32_t tim_arr,
+    uint32_t pwm_width_min_us,
+    uint32_t pwm_width_max_us)
 {
-    s->tim_ccr_min = *s->tim_arr * pwm_width_min_us / SERVO_PERIOD_PWM_US;
-    uint32_t tim_ccr_max = *s->tim_arr * pwm_width_max_us / SERVO_PERIOD_PWM_US;
-    s->tim_ccr_range = tim_ccr_max - s->tim_ccr_min;
+    struct tim *tim = &s->tim;
+    tim->ccr_val_min = tim_arr * pwm_width_min_us / SERVO_PERIOD_PWM_US;
+    uint32_t ccr_val_max = tim_arr * pwm_width_max_us / SERVO_PERIOD_PWM_US;
+    tim->ccr_vals_range = ccr_val_max - tim->ccr_val_min;
 }
 
 void servo_move(struct servo *s, uint32_t deg)
 {
-    uint32_t ccr_val = s->tim_ccr_range * deg / 180 + s->tim_ccr_min;
-    *s->tim_ccr = ccr_val;
+    struct tim *tim = &s->tim;
+    uint32_t ccr_val = tim->ccr_vals_range * deg / 180 + tim->ccr_val_min;
+    *tim->ccr = ccr_val;
 }
